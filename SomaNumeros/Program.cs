72d7cb85d6a4716fa6dc19283ce84e6cda07257b
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 16:06:25 +0100

    Adicionar arquivos de projeto.

---
+++ SomaNumeros/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SomaNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
