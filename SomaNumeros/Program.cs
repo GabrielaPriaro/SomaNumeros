efb983e3332ad1c86b75ab3ce03d7e029a395785
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 16:10:33 +0100

    Implementaçao para calculo de soma de dois valores.

--- SomaNumeros/Program.cs
+++ SomaNumeros/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SomaNumeros
+﻿//Problema "soma"
+//Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela o valor da soma destes
+//números. 
+
+namespace SomaNumeros
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x, y, soma;
+
+            Console.Write("Digite o valor de X: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y: ");
+            y = int.Parse(Console.ReadLine());
+
+            soma = x + y;
+
+            Console.WriteLine($"SOMA = {soma} ");
         }
     }
 }
